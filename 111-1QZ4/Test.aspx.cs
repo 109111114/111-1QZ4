4a415b63bc36fbea41037271e91c78f197a56aba
109111114 <a0921567518@gmail.com>
2022-12-04 15:51:18 +0800

    完成1

---
+++ 111-1QZ4/Test.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1QZ4
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string s_ConnS = "";
+
+            s_ConnS = "Data Source=(localdb)\\MSSQLLocalDB;" +
+            "Initial Catalog=Test;" +
+            "Integrated Security=True;" +
+            "Connect Timeout=30;" +
+            "Encrypt=False;" +
+            "TrustServerCertificate=False;" +
+            "ApplicationIntent=ReadWrite;" +
+            "MultiSubnetFailover=False;" +
+            "User ID = sa; Password = 12345";
+
+            try
+            {
+                SqlConnection o_Conn = new SqlConnection(s_ConnS);
+
+                SqlCommand o_Com = new SqlCommand("Select * from Users", o_Conn);
+                o_Conn.Open();
+                SqlDataReader o_R = o_Com.ExecuteReader();
+                for (; o_R.Read();)
+                {
+                    for (int i_Col=0; i_Col< o_R.FieldCount; i_Col++)
+                    {
+                        Response.Write("&nbsp;&nbsp;" + o_R[i_Col].ToString());
+                    }
+                    Response.Write("<br />");
+                }
+                o_Conn.Close();
+            }
+            catch (Exception o_Exc)
+            {
+                Response.Write(o_Exc.ToString());   
+            }
+        }
+    }
+}
